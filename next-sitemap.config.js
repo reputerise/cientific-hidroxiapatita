a55a9e2319edf4d59e7d9d90c8494655c95ac053
dpipke-setandforget <daniela@setandforget.io>
2025-10-05 17:01:45 -0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://blog.cientific.com.ar/',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
+    outDir: './public',
+    robotsTxtOptions: {
+      policies: [{ userAgent: '*', allow: '/' }],
+    },
+    additionalPaths: async () => {
+      const dynamicRoutes = await fetchDynamicRoutes();
+      return dynamicRoutes;
+    },
+  };
+  
+  async function fetchDynamicRoutes() {
+    const res = await fetch(
+      'https://c38gqpt0.api.sanity.io/v1/data/query/production?query=*[_type == "post"]{slug}'
+    );
+    const posts = await res.json();
+    return posts.result.map(post => ({
+      loc: `/${post.slug.current}`,
+      lastmod: new Date().toISOString(),
+    }));
+  }
+  
