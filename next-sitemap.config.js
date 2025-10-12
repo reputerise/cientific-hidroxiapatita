fa5abec9b42f16b1999edadc2314818b0dbf359b
dpipke-setandforget <daniela@setandforget.io>
2025-10-12 13:45:21 -0300

    added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -16,7 +16,7 @@ module.exports = {
   
   async function fetchDynamicRoutes() {
     const res = await fetch(
-      'https://c38gqpt0.api.sanity.io/v1/data/query/production?query=*[_type == "post"]{slug}'
+        'https://c38gqpt0.api.sanity.io/v1/data/query/production?query=*[_type == "post" && category == "Hidroxiapatita"]{slug}'
     );
     const posts = await res.json();
     return posts.result.map(post => ({
